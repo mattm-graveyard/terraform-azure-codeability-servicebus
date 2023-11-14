7f3854cb0ea6daacfc3a1aec22816b1513559b7c
Matthew MacDonald <matthew.macdonald@me.com>
2023-11-14 17:33:19 -0600

    Codeability First Commit

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "storage_account_id" {
   value = azurerm_storage_account.upload_storage_account.id
+  sensitive = true
 }
 
 output "service_bus_id" {
   value = azurerm_servicebus_namespace.upload_queue_ns.id
+  sensitive = true
 }
 
 output "event_grid_id" {
   value = azurerm_eventgrid_event_subscription.upload_subscription.id
+  sensitive = true
 }
--- storage-account.tf
+++ storage-account.tf
@@ -1,4 +1,4 @@
-resource "azurerm_storage_account" "upload_storage_account" {
+resource "azurerm_storage_account" "upload_storage_account" {                    
   name = "uploadstorageaccount"
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
